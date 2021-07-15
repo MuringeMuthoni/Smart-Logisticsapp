69f7a47916c990890b3f863cbc44f9986b9844d4
tonykaigai <tonykaigai@gmail.com>
2021-07-15 16:52:46 +0300

    up

--- app/tracking-dashboard/tracking-dashboard.page.ts
+++ app/tracking-dashboard/tracking-dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tracking-dashboard',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackingDashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  gotodashboard()
+  {
+    this.router.navigate(['/vehicle-list']); 
+    
+  }
 }
