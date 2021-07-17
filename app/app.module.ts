0085a7d9f5f9baf0bb86712447b9fc75b3c8fc27
tonykaigai <tonykaigai@gmail.com>
2021-07-17 13:08:26 +0300

    up

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,13 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [ 
+  providers: [ SQLite,
+
+
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
